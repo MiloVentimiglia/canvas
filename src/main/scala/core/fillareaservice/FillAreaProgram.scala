84aa57757d09bf514f66e8078dc0598b708c0426
MiloVentimiglia <milo.venti84@gmail.com>
2022-02-15 21:54:05 +0100

    explanations

--- src/main/scala/core/fillareaservice/FillAreaProgram.scala
+++ src/main/scala/core/fillareaservice/FillAreaProgram.scala
@@ -37,6 +37,7 @@ final class FillAreaProgram(val program: DrawingProgram) {
 
   private def inc(iteration: Double): Double = iteration + 1
 
+  /** Creates a list with the coordinates from the surrounding nodes */
   private def nodesCoordinates(x: Int, y: Int): List[Coordinates] = {
     List((x - 1, y + 1),
       (x, y + 1),
@@ -48,6 +49,14 @@ final class FillAreaProgram(val program: DrawingProgram) {
       (x + 1, y - 1))
   }
 
+  /** Creates a map of node coordinates and respective colour. Example:
+   *
+   * (2,2) -> "EMPTY"
+   * (2,1) -> "COLORED"
+   * (2,3) -> "BLOCKED"
+   * (1,3) -> "COLORED"
+   * ....
+   * */
   private def mapNodes(list: List[Coordinates], colour: String): Map[Coordinates, String] = {
     var map = Map[Coordinates, String]()
 
@@ -65,6 +74,10 @@ final class FillAreaProgram(val program: DrawingProgram) {
     map
   }
 
+  /**
+   * Filters out the map for empty and already colored nodes. If empty nodes exists it takes one at random.
+   * Otherwise, if there are no empty nodes, then picks a colored node at random.
+   */
   private def getNextPoint(mappedNodes: Map[Coordinates, String]): (Coordinates, String) = {
     val nodesNoColour: Seq[Coordinates] = mappedNodes.toList.filter(_._2.contains(EMPTY_NODE)).map(_._1)
     val nodesColour: Seq[Coordinates] = mappedNodes.toList.filter(_._2.contains(COLORED_NODE)).map(_._1)
@@ -82,6 +95,22 @@ final class FillAreaProgram(val program: DrawingProgram) {
     }
   }
 
+
+  /**
+   * Splits the horizontal line on the x coordinate into two segments: left and right.
+   * The empty nodes on the trailing of left segment are filled with the colour.
+   * Conversely, the empty nodes on heading of the right segment are filled with the
+   * colour.
+   *
+   *  -----------------
+   *  |           xxxx|        left seg.           right seg.
+   *  |xxxxx   Ö  x  x| ---> List(|xxxxx   )  +  List(  x  x|)  ---> List(|xxxxxooo)  +  List(oox  x|)
+   *  |    x      xxxx|
+   *  |    x          |
+   *  -----------------
+   *
+   *
+   * */
   private def fillingPoint(x: Int, y: Int, colour: String): Unit = {
     val left = program.canvas(y).substring(0, x)
     val right = program.canvas(y).substring(x, program.canvasWidth)
@@ -89,6 +118,10 @@ final class FillAreaProgram(val program: DrawingProgram) {
     program.canvas(y) = fillTrailing(left, colour) + fillHeading(right, colour)
   }
 
+  /**
+   * Regular expression that enables to fill the whitespaces in the trailing of the
+   * left segment with the colour.
+   * */
   private def fillTrailing(line: String, colour: String): String = {
     val pattern = "\\s+".r
     Try{
@@ -99,6 +132,10 @@ final class FillAreaProgram(val program: DrawingProgram) {
     }
   }
 
+  /**
+   * Regular expression that enables to fill the whitespaces in the heading of the
+   * right segment with the colour.
+   * */
   private def fillHeading(line: String, colour: String): String = {
     val pattern = "^\\s+".r
     Try {
