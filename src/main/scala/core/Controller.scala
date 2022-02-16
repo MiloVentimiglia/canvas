1df98f748f3adab80c40ee3c13f12e0fae39a480
MiloVentimiglia <milo.venti84@gmail.com>
2022-02-16 12:31:17 +0100

    Add case the line is empty

--- src/main/scala/core/Controller.scala
+++ src/main/scala/core/Controller.scala
@@ -12,25 +12,25 @@ trait Controller extends Implicits {
                 (implicit program: DrawingProgram, fillAreaProgram: FillAreaProgram): Unit = {
 
     val inputTokens: List[String] = input.split(" ").filter(_.nonEmpty).toList
-    val command: String = inputTokens.head
-    val coordinates: List[String] = inputTokens.tail
+    val command: Option[String] = inputTokens.headOption
 
-    val numberInputs: Int = coordinates.length
+    lazy val coordinates: List[String] = inputTokens.tail
+    lazy val numberInputs: Int = coordinates.length
     lazy val in1: String = coordinates(0)
     lazy val in2: String = coordinates(1)
     lazy val in3: String = coordinates(2)
     lazy val in4: String = coordinates(3)
 
     command match {
-      case "C" if numberInputs == 2 =>
+      case Some("C") if numberInputs == 2 =>
         CreateCanvas(in1, in2).safeRun
-      case "L" if numberInputs == 4 =>
+      case Some("L") if numberInputs == 4 =>
         DrawLine(in1, in2, in3, in4).safeRun
-      case "R" if numberInputs == 4 =>
+      case Some("R") if numberInputs == 4 =>
         DrawSquare(in1, in2, in3, in4).safeRun
-      case "B" if numberInputs == 3 =>
+      case Some("B") if numberInputs == 3 =>
         FillArea(in1, in2, in3).safeRun
-      case "Q" =>
+      case Some("Q") =>
         println("Gracefully closing program.")
         break()
       case _ =>
